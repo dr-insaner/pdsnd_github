25d2050f7edb1d00499c911b302692a1551ffaa3
Jan <jan.hupfeld@gmail.com>
2020-10-21 11:07:02 +0200

    docs: In bikeshare_2.py -> added docstring
    to function time_stats definition

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -90,7 +90,11 @@ def load_data(city, month, day):
 
 
 def time_stats(df):
-    """Displays statistics on the most frequent times of travel."""
+    """Displays statistics on the most frequent times of travel.
+
+    Args:
+        df ([Dataframe]): already filtered dataframe
+    """
 
     print('\nCalculating The Most Frequent Times of Travel...\n')
     start_time = time.time()
