01f79cce689458b6ef63f1ac88d2a08c5f46c885
Jan <jan.hupfeld@gmail.com>
2020-10-21 11:23:53 +0200

    changes tp bikeshare_2.py -> added print bye bye

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -199,6 +199,8 @@ def main():
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
             break
+        else:
+            print('bye bye')
 
 
 if __name__ == "__main__":
